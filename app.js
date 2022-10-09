990827823479833d5d1cc19407e9b37fe2096e62
Fanny Gautier <fannygautier92@gmail.com>
2022-10-09 16:38:51 +0200

    :sparkles: add selection pour les projets en js

--- app.js
+++ app.js
@@ -10,3 +10,35 @@ document.addEventListener("scroll",function(){
     console.log(intElemScrollTop);
 
 })
+
+let javaScriptProject=document.querySelectorAll(".js");
+let htmlProject=document.querySelectorAll(".html");
+let phpProject=document.querySelectorAll(".php");
+
+let buttonJavaScript=document.querySelector(".javaScript-button")
+let buttonPhp=document.querySelector(".php-button")
+let buttonHtml=document.querySelector(".html-button")
+// Input -> un DOm object 
+//Sort -> dom event (display none)
+function  TriProjet(Programm,choosenprogramm){
+   console.log(Programm,choosenprogramm)
+Programm.forEach(e => {
+    if(e.classList.contains(choosenprogramm)){
+        e.style.display="none";
+        }
+});   
+
+}
+
+
+buttonJavaScript.addEventListener("click",() => {
+    TriProjet(javaScriptProject,"js");
+})
+
+buttonPhp.addEventListener("click",() => {
+    TriProjet(phpProject,"php");
+})
+
+buttonHtml.addEventListener("click",() => {
+    TriProjet(htmlProject,"html");
+})
